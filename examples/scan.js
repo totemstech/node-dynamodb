712c5faf1f745c1e52e67494827ad48e9d14ce1d
Pablo Cantero <pablo@pablocantero.com>
2012-01-25 12:44:56 -0200

    Adding scan

---
+++ examples/scan.js
@@ -0,0 +1,45 @@
+// Copyright Stanislas Polu and other Contributors
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+/**
+ * scan.js
+ *
+ * http://docs.amazonwebservices.com/amazondynamodb/latest/developerguide/API_Scan.html
+ * 
+ * Command: Scan
+ * @param table [string] the table name
+ * @param options the DynamoDB options for Scan as a dictionary
+ */
+
+var ddb = require('../lib/ddb.js').ddb({ accessKeyId: '',
+secretAccessKey: '' });
+
+
+ddb.scan('test', function(err, res) {
+    if(err) {
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+});
+
+
+
